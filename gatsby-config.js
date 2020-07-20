4991607b802d2d276d09b9e5ae11003bc6f9f420
jromest <jromestiller@gmail.com>
2020-07-20 13:04:09 +0800

    :tada: initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,74 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Netlify CMS`,
+    author: {
+      name: `Jrom`,
+      summary: `who likes building useful things.`
+    },
+    description: `A starter CMS demonstrating what Gatsby and Netlify CMS can do.`,
+    siteUrl: `https://gatsby-netlifycms.netlify.app/`,
+    social: {
+      twitter: `jromest_`
+    }
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/posts`,
+        name: `posts`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/img`,
+        name: `images`
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590
+            }
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`
+            }
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`
+        ]
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Netlify CMS`,
+        short_name: `Gatsby Netlify CMS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `static/img/gatsby-icon.png`
+      }
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`
+      }
+    },
+    `gatsby-plugin-netlify-cms`
+  ]
+}
